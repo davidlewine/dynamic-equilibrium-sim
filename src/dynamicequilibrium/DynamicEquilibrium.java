c7c19331320f6c5b9ddc5bb813296bc333b193e7
dlewine <dlewine@derryfield.org>
2015-04-13 13:04:09 -0400

    separate classes for each substance derived from Particle class. Separate lists for each substance to keep track of particles.
    added hashmap to keep track of particle lists.
    particles only search their bin and surrounding bins(a bin holds the particles in an nxn region) for particles to react with.

--- src/dynamicequilibrium/DynamicEquilibrium.java
+++ src/dynamicequilibrium/DynamicEquilibrium.java
@@ -30,51 +30,64 @@ public class DynamicEquilibrium extends JPanel {
     ArrayList<Particle> clList = new ArrayList();
     ArrayList<Particle> c6h8o7List = new ArrayList();
     ArrayList<Particle> c6h5o7List = new ArrayList();
+    Hashtable<String, ArrayList<Particle>> particleLists = new Hashtable(20);
     int width, height;
     double atomRad;
+    Bins bins;
 
     public DynamicEquilibrium(int w, int h) {
         width = w;
         height = h;
-        for (int i = 0; i < 0; i++) {
+        bins = new Bins(w, h);
+        particleLists.put("HCl", hclList);
+        particleLists.put("NaHCO3", nahco3List);
+        particleLists.put("H2O", h2oList);
+        particleLists.put("HCO3", hco3List);
+        particleLists.put("C6H8O7", c6h8o7List);
+        particleLists.put("Na", naList);
+        particleLists.put("Cl", clList);
+        particleLists.put("H", hList);
+        
+        for (int i = 0; i < 20; i++) {
             Particle tempParticle = new HCl(Math.random() * width, Math.random() * height);
             //particles.add(tempParticle);
             hclList.add(tempParticle);
+            bins.add(tempParticle);
         }
         for (int i = 0; i < 75; i++) {
             Particle tempParticle = new NaHCO3(Math.random() * width, Math.random() * height);
             //particles.add(tempParticle);
             nahco3List.add(tempParticle);
+            bins.add(tempParticle);
         }
         for (int i = 0; i < 20; i++) {
             Particle tempParticle = new C6H8O7(Math.random() * width, Math.random() * height);
             //particles.add(tempParticle);
             c6h8o7List.add(tempParticle);
+            bins.add(tempParticle);
         }
     }
 
     public void update() {
         moveParticles();
-        react();
+        reactParticles();
     }
 
     public void moveParticles() {
-        moveList(nahco3List);
-        moveList(hclList);
-        moveList(h2oList);
-        moveList(hList);
-        moveList(naList);
-        moveList(clList);
-        moveList(hco3List);
-        moveList(c6h8o7List);
-        moveList(c6h5o7List);
+        for (Enumeration e = particleLists.keys(); e.hasMoreElements();){
+            ArrayList<Particle> list = particleLists.get( e.nextElement() );
+            moveList(list);
+            //System.out.println("moveList" + list);
+        }
     }
 
     public void moveList(ArrayList<Particle> list) {
         Iterator<Particle> iter = list.iterator();
         while (iter.hasNext()) {
             Particle particle = iter.next();
+            bins.remove(particle);
             particle.move();
+            bins.add(particle);
             if (particle.pos.x() <= 0 || particle.pos.x() >= width) {
                 particle.vel = new Vector2D(-particle.vel.x(), particle.vel.y());
             }
@@ -83,109 +96,26 @@ public class DynamicEquilibrium extends JPanel {
             }
         }
     }
-
-    public void react() {
-        //nahco3 reactions
-        Iterator<Particle> iter = nahco3List.iterator();
-        while (iter.hasNext()) {
-            Particle particle = iter.next();
-            if (Math.random() > .996) {
-                naList.add(new Na(Math.min(particle.pos.x() + 1 * particle.r, width), particle.pos.y()));
-                hco3List.add(new HCO3(Math.max(particle.pos.x() - 1 * particle.r, 0), particle.pos.y()));
-                iter.remove();
-            }
-        }
-
-        //hcl reactions
-        iter = hclList.iterator();
-        while (iter.hasNext()) {
-            Particle particle = iter.next();
-            if (Math.random() > .995) {
-                hList.add(new H(Math.min(particle.pos.x() + 1 * particle.r, width), particle.pos.y()));
-                clList.add(new Cl(Math.max(particle.pos.x() - 1 * particle.r, 0), particle.pos.y()));
-                iter.remove();
-            }
-        }
-        
-        //C6H8O7 reactions
-        iter = c6h8o7List.iterator();
-        while (iter.hasNext()) {
-            Particle particle = iter.next();
-            if(particle.charge > -3){
-                if (Math.random() > .998) {
-                    hList.add(new H(Math.min(particle.pos.x() + 1 * particle.r, width), particle.pos.y()));
-//                    hList.add(new Na(particle.pos.x(), Math.min(particle.pos.y() + 1 * particle.r, height)));
-//                    hList.add(new Na(particle.pos.x(), Math.max(particle.pos.y() - 1 * particle.r, height)));
-                    particle.charge--;
-//                    iter.remove();
-                }
-            }
-        }
-
-        //h reactions
-        iter = hList.iterator();
-        whileIter:
-        while (iter.hasNext()) {
-            Particle pa = iter.next();
-            Iterator<Particle> iterB = clList.iterator();
-            while (iterB.hasNext()) {
-                Particle pb = iterB.next();
-                if ((pa.pos.minus(pb.pos)).norm() < pa.r + pb.r) {//if pa and pb are touching
-                    if (Math.random() > .996) {
-                        hclList.add(new HCl(pa.pos.x(), pa.pos.y()));
-                        iterB.remove();
-                        iter.remove();
-                        continue whileIter;
+    
+    public void reactParticles() {
+        for (Enumeration e = particleLists.keys(); e.hasMoreElements();){
+            ArrayList<Particle> list = particleLists.get( e.nextElement() );
+            Iterator<Particle> iter = list.iterator();
+            while (iter.hasNext()) {
+                Particle particle = iter.next();
+                ArrayList<Particle> products = particle.react();
+                if(products.size() > 0){
+                    bins.remove(particle);
+                    for(Particle p: products){
+                        particleLists.get(p.formula).add(p);
+                        bins.add(p);
                     }
+                    iter.remove();
                 }
             }
-
-            iterB = hco3List.iterator();
-            while (iterB.hasNext()) {
-                Particle pb = iterB.next();
-                if ((pa.pos.minus(pb.pos)).norm() < pa.r + pb.r) {//if pa and pb are touching
-                    if (Math.random() > .992) {
-                        h2oList.add(new H2O(pa.pos.x(), pa.pos.y()));
-                        iterB.remove();
-                        iter.remove();
-                        continue whileIter;
-                    }
-                }
-            }
-            
-            iterB = c6h8o7List.iterator();
-            while (iterB.hasNext()) {
-                Particle pb = iterB.next();
-                if (pb.charge < 0 && ((pa.pos.minus(pb.pos)).norm() < pa.r + pb.r)) {//if pa and pb are touching
-                    if (Math.random() > .992) {
-                        pb.charge++;
-                        iter.remove();
-                        continue whileIter;
-                    }
-                }
-            }
-        }
-
-        //Na reactions
-        iter = naList.iterator();
-        whileIter:
-        while (iter.hasNext()) {
-            Particle pa = iter.next();
-            Iterator<Particle> iterB = hco3List.iterator();
-            while (iterB.hasNext()) {
-                Particle pb = iterB.next();
-                if ((pa.pos.minus(pb.pos)).norm() < pa.r + pb.r) {//if pa and pb are touching
-                    if (Math.random() > .997) {
-                        nahco3List.add(new NaHCO3(pa.pos.x(), pa.pos.y()));
-                        iterB.remove();
-                        iter.remove();
-                        continue whileIter;
-                    }
-                }
-            }
-        }
-
+        }     
     }
+
 
     public void draw(Graphics g) {
         for (Particle p : nahco3List) {
