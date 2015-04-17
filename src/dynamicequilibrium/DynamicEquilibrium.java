938dabe78de165d892cd49bfec27e0984082cf40
dlewine <dlewine@derryfield.org>
2015-04-16 21:04:04 -0400

    separate classes for each substance derived from Particle class. Separate lists for each substance to keep track of particles.
    added hashmap to keep track of particle lists.
    particles only search their bin and surrounding bins(a bin holds the particles in an nxn region) for particles to react with.

--- src/dynamicequilibrium/DynamicEquilibrium.java
+++ src/dynamicequilibrium/DynamicEquilibrium.java
@@ -30,6 +30,8 @@ public class DynamicEquilibrium extends JPanel {
     ArrayList<Particle> clList = new ArrayList();
     ArrayList<Particle> c6h8o7List = new ArrayList();
     ArrayList<Particle> c6h5o7List = new ArrayList();
+    ArrayList<Particle> c6h6o7List = new ArrayList();
+    ArrayList<Particle> c6h7o7List = new ArrayList();
     Hashtable<String, ArrayList<Particle>> particleLists = new Hashtable(20);
     int width, height;
     double atomRad;
@@ -44,23 +46,26 @@ public class DynamicEquilibrium extends JPanel {
         particleLists.put("H2O", h2oList);
         particleLists.put("HCO3", hco3List);
         particleLists.put("C6H8O7", c6h8o7List);
+        particleLists.put("C6H7O7", c6h7o7List);
+        particleLists.put("C6H6O7", c6h6o7List);
+        particleLists.put("C6H5O7", c6h5o7List);
         particleLists.put("Na", naList);
         particleLists.put("Cl", clList);
         particleLists.put("H", hList);
         
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 1000; i++) {
             Particle tempParticle = new HCl(Math.random() * width, Math.random() * height);
             //particles.add(tempParticle);
             hclList.add(tempParticle);
             bins.add(tempParticle);
         }
-        for (int i = 0; i < 75; i++) {
+        for (int i = 0; i < 1000; i++) {
             Particle tempParticle = new NaHCO3(Math.random() * width, Math.random() * height);
             //particles.add(tempParticle);
             nahco3List.add(tempParticle);
             bins.add(tempParticle);
         }
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 0; i++) {
             Particle tempParticle = new C6H8O7(Math.random() * width, Math.random() * height);
             //particles.add(tempParticle);
             c6h8o7List.add(tempParticle);
@@ -85,62 +90,121 @@ public class DynamicEquilibrium extends JPanel {
         Iterator<Particle> iter = list.iterator();
         while (iter.hasNext()) {
             Particle particle = iter.next();
+            if(particle.bin[0] == -1){
+                System.out.println("-1: " + particle);
+            }
             bins.remove(particle);
             particle.move();
+            
+            if (particle.pos.x() < 0){ 
+                particle.vel = new Vector2D(Math.abs(particle.vel.x()), particle.vel.y());
+                particle.pos = new Vector2D(0, particle.pos.y());
+            }
+            if(particle.pos.x() > width) {
+                particle.vel = new Vector2D(-1*Math.abs(particle.vel.x()), particle.vel.y());
+                particle.pos = new Vector2D(width, particle.pos.y());
+            }
+            if (particle.pos.y() < 0){ 
+                particle.vel = new Vector2D(particle.vel.x(), Math.abs(particle.vel.y()));
+                particle.pos = new Vector2D(particle.pos.x(), 0);
+            }
+            if(particle.pos.y() > height) {
+                particle.vel = new Vector2D(particle.vel.x(), -1 * Math.abs(particle.vel.y()));
+                particle.pos = new Vector2D(particle.pos.x(), height);
+            }
             bins.add(particle);
-            if (particle.pos.x() <= 0 || particle.pos.x() >= width) {
-                particle.vel = new Vector2D(-particle.vel.x(), particle.vel.y());
-            }
-            if (particle.pos.y() <= 0 || particle.pos.y() >= height) {
-                particle.vel = new Vector2D(particle.vel.x(), -particle.vel.y());
-            }
+            
+            
         }
     }
     
     public void reactParticles() {
+        //to do: search for neighbors and react with first neighbor.
         for (Enumeration e = particleLists.keys(); e.hasMoreElements();){
             ArrayList<Particle> list = particleLists.get( e.nextElement() );
-            Iterator<Particle> iter = list.iterator();
-            while (iter.hasNext()) {
-                Particle particle = iter.next();
-                ArrayList<Particle> products = particle.react();
-                if(products.size() > 0){
-                    bins.remove(particle);
-                    for(Particle p: products){
-                        particleLists.get(p.formula).add(p);
-                        bins.add(p);
+            int listIndex = 0;//this cant be an iterator because the neighbor might be of the same type 
+                               //and then the neighbor and the particle will both need to be removed from 
+                               //the same list, i.e. Concurrent modification.
+            while (listIndex < list.size()) {
+                Particle particle = list.get(listIndex);
+                ArrayList<Particle> products = new ArrayList();
+//                if(products.size() > 0){
+//                    for(Particle p: products){
+//                        particleLists.get(p.formula).add(p);
+//                        bins.add(p);
+//                        //System.out.println("part: " + particle.formula + " prod: " + p.formula);
+//                    }
+//                    //System.out.println("remove: " + particle + " ex: " + particle.bin[0] + " " + particle.bin[1]);   
+//                    bins.remove(particle);
+//                    list.remove(particle);
+//                    listIndex--; 
+//                }
+//                else{
+                
+                   ArrayList<Particle> neighbors = bins.getNeighbors(particle);
+                   Iterator<Particle> neighborsIter = neighbors.iterator();
+                   while(neighborsIter.hasNext() && products.size()==0){
+                       Particle neighbor = neighborsIter.next();
+                       if(neighbor != particle){
+                       products = particle.react(neighbor);
+                       if(products.size() > 0){
+                           //System.out.println("part: " + particle + " neighbor: " + neighbor);
+                            for(Particle p: products){
+                                particleLists.get(p.formula).add(p);
+                                bins.add(p);
+                                //System.out.println("part: " + particle + " neighbor: " + neighbor + " prod: " + p);
+                            }
+                            bins.remove(neighbor);
+                            particleLists.get(neighbor.formula).remove(neighbor); 
+                        }
                     }
-                    iter.remove();
+                   }
+                   if(products.size() > 0){
+                       bins.remove(particle);
+                       list.remove(particle);
+                       listIndex--;
+                   }
+                   listIndex++; 
                 }
+                 
             }
-        }     
+           
     }
 
 
     public void draw(Graphics g) {
-        for (Particle p : nahco3List) {
-            p.display(g);
-        }
-        for (Particle p : hclList) {
-            p.display(g);
-        }
+//        for (Particle p : nahco3List) {
+//            p.display(g);
+//        }
+//        for (Particle p : hclList) {
+//            p.display(g);
+//        }
         for (Particle p : hList) {
             p.display(g);
         }
-        for (Particle p : clList) {
-            p.display(g);
-        }
-        for (Particle p : naList) {
-            p.display(g);
-        }
+//        for (Particle p : clList) {
+//            p.display(g);
+//        }
+//        for (Particle p : naList) {
+//            p.display(g);
+//        }
         for (Particle p : hco3List) {
             p.display(g);
         }
         for (Particle p : h2oList) {
             p.display(g);
         }
-        for (Particle p : c6h8o7List) {
-            p.display(g);
-        }
+//        for (Particle p : c6h8o7List) {
+//            p.display(g);
+//        }
+//        for (Particle p : c6h7o7List) {
+//            p.display(g);
+//        }
+//        for (Particle p : c6h6o7List) {
+//            p.display(g);
+//        }
+//        for (Particle p : c6h5o7List) {
+//            p.display(g);
+//        }
     }
 }
